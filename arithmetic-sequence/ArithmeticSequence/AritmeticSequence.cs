6c3ba2a2785942e677fb993c091eba2ae599858c
L3b1n <binleo07@icloud.com>
2023-09-08 18:33:29 +0300

    Add task (arithmetic sequence) of education practice

---
+++ arithmetic-sequence/ArithmeticSequence/AritmeticSequence.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace ArithmeticSequenceTask
+{
+    public static class ArithmeticSequence
+    {
+        public static int Calculate(int number, int add, int count)
+        {
+            int fins = number;
+            int sum = number;
+            int i = 0;
+            count--;
+            while (i < count)
+            {
+                sum += add;
+                i++;
+                fins += sum;
+            }
+
+            if (((number == int.MaxValue) && (add > 0)) || ((number == int.MinValue) && (add < 0)))
+            {
+                throw new OverflowException();
+            }
+
+            if (count < 0)
+            {
+                throw new ArgumentException();
+            }
+
+            return fins;
+        }
+    }
+}
