e60a7d22f31cf13af12b8434efa68005494ca38c
L3b1n <binleo07@icloud.com>
2023-09-10 16:12:17 +0300

    Add task (bools) of educational practice

---
+++ bools/Bools/Booleans.cs
@@ -0,0 +1,16 @@
+﻿namespace Bools
+{
+    public static class Booleans
+    {
+        public static bool ReturnTrue()
+        {
+            return true;
+        }
+
+        public static bool ReturnFalse()
+        {
+            return false;
+            throw new NotImplementedException();
+        }
+    }
+}
