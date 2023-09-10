d996d2d5e985f2d0e77907b535bf02d6d66ded1d
L3b1n <binleo07@icloud.com>
2023-09-10 16:13:46 +0300

    Add tests for task (bools) of educational practice

---
+++ bools/Bools.Tests/BooleansTests.cs
@@ -0,0 +1,28 @@
+﻿global using NUnit.Framework;
+
+namespace Bools.Tests
+{
+    [TestFixture]
+    public class BooleansTests
+    {
+        [Test]
+        public static void ReturnTrue()
+        {
+            // Act
+            var actualResult = Bools.Booleans.ReturnTrue();
+
+            // Assert
+            Assert.IsTrue(actualResult);
+        }
+
+        [Test]
+        public static void ReturnFalse()
+        {
+            // Act
+            var actualResult = Bools.Booleans.ReturnFalse();
+
+            // Assert
+            Assert.IsFalse(actualResult);
+        }
+    }
+}
