9d7f7cc7fe00a9d104821991bd51cadb55bc1b53
L3b1n <binleo07@icloud.com>
2023-09-10 16:12:07 +0300

    Add task (bools) of educational practice

---
+++ bools/Bools/LogicalPuzzles.cs
@@ -0,0 +1,40 @@
+﻿namespace Bools
+{
+    public static class LogicalPuzzles
+    {
+        public static bool Puzzle1(bool b1, bool b2)
+        {
+            return !b1 || b2;
+        }
+
+        public static bool Puzzle2(bool b1, bool b2)
+        {
+            return !b1 && b2;
+        }
+
+        public static bool Puzzle3(bool b1, bool b2)
+        {
+            return b1 || !b2;
+        }
+
+        public static bool Puzzle4(bool b1, bool b2)
+        {
+            return b1 && !b2;
+        }
+
+        public static bool Puzzle5(bool b1, bool b2)
+        {
+            return !(b1 || b2);
+        }
+
+        public static bool Puzzle6(bool b1, bool b2)
+        {
+            return !(b1 && b2);
+        }
+
+        public static bool Puzzle7(bool b1, bool b2)
+        {
+            return b1 == b2;
+        }
+    }
+}
