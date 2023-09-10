958e97586880defb147831d0cdd6c9fc22d9d615
L3b1n <binleo07@icloud.com>
2023-09-10 16:12:12 +0300

    Add task (bools) of educational practice

---
+++ bools/Bools/LogicalOperators.cs
@@ -0,0 +1,55 @@
+﻿namespace Bools
+{
+    public static class LogicalOperators
+    {
+        public static bool LogicalAnd1(bool b1, bool b2)
+        {
+            return b1 && b2;
+        }
+
+        public static bool LogicalAnd2(bool b1, bool b2, bool b3)
+        {
+            return b1 && b2 && b3;
+        }
+
+        public static bool LogicalAnd3(bool b1, bool b2, bool b3, bool b4)
+        {
+            return b1 && b2 && b3 && b4;
+        }
+
+        public static bool LogicalOr1(bool b1, bool b2)
+        {
+            return b1 || b2;
+        }
+
+        public static bool LogicalOr2(bool b1, bool b2, bool b3)
+        {
+            return b1 || b2 || b3;
+        }
+
+        public static bool LogicalOr3(bool b1, bool b2, bool b3, bool b4)
+        {
+            return b1 || b2 || b3 || b4;
+        }
+
+        public static bool LogicalXor1(bool b1, bool b2)
+        {
+            return b1 ^ b2;
+        }
+
+        public static bool LogicalXor2(bool b1, bool b2, bool b3)
+        {
+            return b1 ^ b2 ^ b3;
+        }
+
+        public static bool LogicalXor3(bool b1, bool b2, bool b3, bool b4)
+        {
+            return b1 ^ b2 ^ b3 ^ b4;
+        }
+
+        public static bool Negate(bool b)
+        {
+            return !b;
+        }
+    }
+}
