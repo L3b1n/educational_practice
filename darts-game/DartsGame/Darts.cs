48844d68345dd6c33a1a70f6aadb176cf7bb082b
L3b1n <binleo07@icloud.com>
2023-09-14 22:35:34 +0300

    Add task(darts-game) of educational practice

---
+++ darts-game/DartsGame/Darts.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DartsGame
+{
+    public static class Darts
+    {
+        public static int GetScore(double x, double y)
+        {
+            double radius = Math.Sqrt((x * x) + (y * y));
+            if (radius <= 1)
+            {
+                return 10;
+            }
+            else if (radius <= 5)
+            {
+                return 5;
+            }
+            else if (radius <= 10)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
