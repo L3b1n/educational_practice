704458fd3999c472e7ed405404a458577976aba5
L3b1n <binleo07@icloud.com>
2023-09-19 22:40:36 +0300

    Add task (fancy-calc) of educational practice

---
+++ fancy-calc/FancyCalc/Operations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FancyCalc
+{
+    public static class Operations
+    {
+        public static int Plus(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Minus(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            int result = x * y;
+            return result;
+        }
+
+        public static int Sum(int x1, int x2, int x3)
+        {
+            int sum1 = x1 + x2;
+            int sum = sum1 + x3;
+            return sum;
+        }
+    }
+}
