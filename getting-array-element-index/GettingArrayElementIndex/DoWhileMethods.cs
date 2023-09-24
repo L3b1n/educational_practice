b347847dc1d3a807faf41aeeef92fa62e2a7db84
L3b1n <binleo07@icloud.com>
2023-09-24 10:34:03 +0300

    Add task (getting-array-element-index) of educational practice

---
+++ getting-array-element-index/GettingArrayElementIndex/DoWhileMethods.cs
@@ -0,0 +1,143 @@
+﻿namespace GettingArrayElementIndex
+{
+    public static class DoWhileMethods
+    {
+        public static int GetIndexOf(ulong[]? arrayToSearch, ulong value)
+        {
+            if (arrayToSearch is null)
+            {
+                throw new ArgumentNullException(nameof(arrayToSearch));
+            }
+
+            int i = 0;
+            if (arrayToSearch.Length - 1 < 0)
+            {
+                return -1;
+            }
+
+            do
+            {
+                if (arrayToSearch[i] == value)
+                {
+                    return i;
+                }
+            }
+            while (++i < arrayToSearch.Length);
+
+            return -1;
+        }
+
+        public static int GetIndexOf(ulong[]? arrayToSearch, ulong value, int startIndex, int count)
+        {
+            if (arrayToSearch is null)
+            {
+                throw new ArgumentNullException(nameof(arrayToSearch));
+            }
+
+            if (startIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex is less than zero");
+            }
+
+            if (startIndex > arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex is greater than arrayToSearch.Length");
+            }
+
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "count is less than zero");
+            }
+
+            int lastPosition = startIndex + count;
+            if (lastPosition > arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "startIndex + count > arrayToSearch.Length");
+            }
+
+            int i = startIndex;
+            while (i < lastPosition)
+            {
+                if (arrayToSearch[i] == value)
+                {
+                    return i;
+                }
+
+                i++;
+            }
+
+            return -1;
+        }
+
+        public static int GetLastIndexOf(ulong[]? arrayToSearch, ulong value)
+        {
+            if (arrayToSearch is null)
+            {
+                throw new ArgumentNullException(nameof(arrayToSearch));
+            }
+
+            int i = arrayToSearch.Length - 1;
+            if (i < 0)
+            {
+                return -1;
+            }
+
+            do
+            {
+                if (arrayToSearch[i] == value)
+                {
+                    return i;
+                }
+            }
+            while (--i >= 0);
+
+            return -1;
+        }
+
+        public static int GetLastIndexOf(ulong[]? arrayToSearch, ulong value, int startIndex, int count)
+        {
+            if (arrayToSearch is null)
+            {
+                throw new ArgumentNullException(nameof(arrayToSearch));
+            }
+
+            if (startIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex is less than zero");
+            }
+
+            if (startIndex > arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), "startIndex is greater than arrayToSearch.Length");
+            }
+
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "count is less than zero");
+            }
+
+            int lastIndex = startIndex + count;
+            if (lastIndex > arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "startIndex + count > arrayToSearch.Length");
+            }
+
+            int i = lastIndex - 1;
+            if (i < 0)
+            {
+                return -1;
+            }
+
+            do
+            {
+                if (arrayToSearch[i] == value)
+                {
+                    return i;
+                }
+            }
+            while (--i >= startIndex);
+
+            return -1;
+        }
+    }
+}
