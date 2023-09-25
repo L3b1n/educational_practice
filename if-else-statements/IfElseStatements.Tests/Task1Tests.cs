d9cb303066c2ec5a2b60a8d50018b72a7dfdbcb3
L3b1n <binleo07@icloud.com>
2023-09-25 21:44:20 +0300

    Add tests for task (if-else-statements) of educational practice

---
+++ if-else-statements/IfElseStatements.Tests/Task1Tests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace IfStatements.Tests
+{
+    [TestFixture]
+    public class Task1Tests
+    {
+        [TestCase(true, ExpectedResult = 123)]
+        [TestCase(false, ExpectedResult = -123)]
+        public int DoSomething_ReturnsInteger(bool b)
+        {
+            return Task1.DoSomething(b);
+        }
+    }
+}
