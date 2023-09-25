0429e3bb60c0ac393c7ef5ebbbf54111f7121d4f
L3b1n <binleo07@icloud.com>
2023-09-25 21:46:39 +0300

    Add tests for task (if-else-statements) of educational practice

---
+++ if-else-statements/IfElseStatements/Task8.cs
@@ -0,0 +1,48 @@
+﻿namespace IfStatements
+{
+    public static class Task8
+    {
+        public static int DoSomething(int i1, int i2)
+        {
+            int result = 0;
+            if (i1 >= -4 && i1 < 9 && i2 > 5)
+            {
+                result = i1 + i2;
+            }
+            else if (i1 >= -4 && i1 < 9 && i2 <= -3)
+            {
+                result = (i1 * 2) - i2;
+            }
+            else if (i1 >= -4 && i1 < 9 && i2 > -3 && i2 <= 5)
+            {
+                result = i2 - (i1 * i1);
+            }
+            else if (i1 < -4 && i2 >= 7)
+            {
+                result = (i2 * 2) - i1;
+            }
+            else if (i1 < -4 && i2 < -5)
+            {
+                result = i2 - i1;
+            }
+            else if (i1 < -4 && i2 >= -5 && i2 < 7)
+            {
+                result = i1 - i2;
+            }
+            else if (i1 >= 9 && i2 >= 7)
+            {
+                result = i1 - (i2 * i2);
+            }
+            else if (i1 >= 9 && i2 < -7)
+            {
+                result = 2 * (i1 - i2);
+            }
+            else
+            {
+                result = i1 * i2;
+            }
+
+            return result;
+        }
+    }
+}
