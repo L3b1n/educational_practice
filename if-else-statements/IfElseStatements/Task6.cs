429f0b25b7f80db50a33ee45cb5430a15535d9b5
L3b1n <binleo07@icloud.com>
2023-09-25 21:46:51 +0300

    Add tests for task (if-else-statements) of educational practice

---
+++ if-else-statements/IfElseStatements/Task6.cs
@@ -0,0 +1,32 @@
+﻿namespace IfStatements
+{
+    public static class Task6
+    {
+        public static int DoSomething(int i)
+        {
+            if (i < -8)
+            {
+                return i * i;
+            }
+
+            if (i <= -2)
+            {
+                return i * 3;
+            }
+
+            if (i <= 3)
+            {
+                return (i * 2) + (i * i);
+            }
+
+            if (i < 7)
+            {
+                return i * (i - 1) * -1;
+            }
+            else
+            {
+                return i * 2;
+            }
+        }
+    }
+}
