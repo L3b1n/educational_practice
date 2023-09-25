c15b348d7e58526a39e603b9be7a9c7fb29da95b
L3b1n <binleo07@icloud.com>
2023-09-25 21:47:22 +0300

    Add tests for task (if-else-statements) of educational practice

---
+++ if-else-statements/IfElseStatements/Task1.cs
@@ -0,0 +1,17 @@
+﻿namespace IfStatements
+{
+    public static class Task1
+    {
+        public static int DoSomething(bool b)
+        {
+            if (b)
+            {
+                return 123;
+            }
+            else
+            {
+                return -123;
+            }
+        }
+    }
+}
