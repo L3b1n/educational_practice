c7bea0c33dda9a1687964349117f07b9344d1caa
L3b1n <binleo07@icloud.com>
2023-09-26 22:33:40 +0300

    Add task (integers) of educational practice

---
+++ integers/Integers/Integers.cs
@@ -0,0 +1,47 @@
+﻿// ReSharper disable ConvertToConstant.Local
+
+namespace Literals
+{
+    public static class Integers
+    {
+        public static int ReturnInteger11()
+        {
+            return 0;
+        }
+
+        public static int ReturnInteger12()
+        {
+            return 1;
+        }
+
+        public static int ReturnInteger13()
+        {
+            return -1;
+        }
+
+        public static int ReturnInteger14()
+        {
+            return 2_147_483_647;
+        }
+
+        public static int ReturnInteger15()
+        {
+            return -2_147_483_648;
+        }
+
+        public static uint ReturnInteger16()
+        {
+            return 1u;
+        }
+
+        public static uint ReturnInteger17()
+        {
+            return 32_767u;
+        }
+
+        public static uint ReturnInteger18()
+        {
+            return 2_147_483_647u;
+        }
+    }
+}
