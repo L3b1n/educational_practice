a2df94beed36ff8b5883dcf3e7b310afa6c9f551
L3b1n <binleo07@icloud.com>
2023-09-26 22:33:29 +0300

    Add task (integers) of educational practice

---
+++ integers/Integers/Consts.cs
@@ -0,0 +1,45 @@
+﻿namespace Integers
+{
+    public static class Consts
+    {
+        public static int ReturnLargestInteger()
+        {
+            return int.MaxValue;
+        }
+
+        public static int ReturnSmallestInteger()
+        {
+            return int.MinValue;
+        }
+
+        public static uint ReturnLargestUnsignedInteger()
+        {
+            return uint.MaxValue;
+        }
+
+        public static uint ReturnSmallestUnsignedInteger()
+        {
+            return uint.MinValue;
+        }
+
+        public static long ReturnLargestLongInteger()
+        {
+            return long.MaxValue;
+        }
+
+        public static long ReturnSmallestLongInteger()
+        {
+            return long.MinValue;
+        }
+
+        public static ulong ReturnLargestUnsignedLongInteger()
+        {
+            return ulong.MaxValue;
+        }
+
+        public static ulong ReturnSmallestUnsignedLongInteger()
+        {
+            return ulong.MinValue;
+        }
+    }
+}
