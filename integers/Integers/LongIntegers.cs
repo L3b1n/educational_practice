d8e71a0d8b72c44c71349bcf1d461f9995f6f5c7
L3b1n <binleo07@icloud.com>
2023-09-26 22:33:51 +0300

    Add task (integers) of educational practice

---
+++ integers/Integers/LongIntegers.cs
@@ -0,0 +1,65 @@
+﻿namespace Literals
+{
+    public static class LongIntegers
+    {
+        public static long ReturnLongInteger21()
+        {
+            return 4956185095298947214L;
+        }
+
+        public static long ReturnLongInteger22()
+        {
+            return -1280010762458239942L;
+        }
+
+        public static long ReturnLongInteger23()
+        {
+            return -945783496234828465L;
+        }
+
+        public static ulong ReturnLongInteger24()
+        {
+            return 16269823234523742845uL;
+        }
+
+        public static long ReturnLongInteger25()
+        {
+            return 9223372036854775807L;
+        }
+
+        public static long ReturnLongInteger26()
+        {
+            return 773_738_404_492_802_748L;
+        }
+
+        public static ulong ReturnLongInteger27()
+        {
+            return 17_977_307_477_258_691_517uL;
+        }
+
+        public static ulong ReturnLongInteger28()
+        {
+            return 14_193_065_825_095_688_383uL;
+        }
+
+        public static long ReturnLongInteger29()
+        {
+            return 4_100_761_908_933_204_629L;
+        }
+
+        public static long ReturnLongInteger210()
+        {
+            return 1_645_102_583_813_967_509L;
+        }
+
+        public static long ReturnLongInteger211()
+        {
+            return 6_148_914_691_236_517_205L;
+        }
+
+        public static long ReturnLongInteger212()
+        {
+            return 8_446_744_073_709_551_615L;
+        }
+    }
+}
