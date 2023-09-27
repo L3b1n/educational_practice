5528e536045f01464b3edaf132751080bf7f1d9f
L3b1n <binleo07@icloud.com>
2023-09-27 21:25:48 +0300

    Add task (if-statements) of educational practice

---
+++ if-statements/IfStatements/Task11.cs
@@ -0,0 +1,105 @@
+﻿namespace IfStatements
+{
+    public static class Task11
+    {
+        public static int DoSomething(bool b1, bool b2, int i)
+        {
+            if (b1)
+            {
+                if (b2)
+                {
+                    if (i == 0)
+                    {
+                        return 1;
+                    }
+
+                    if (i < -8 || i >= 8)
+                    {
+                        return i;
+                    }
+
+                    if (i >= -4 && i < 4)
+                    {
+                        return i;
+                    }
+
+                    if (i < -4)
+                    {
+                        return i * 3;
+                    }
+                    else
+                    {
+                        return i * 2;
+                    }
+                }
+                else
+                {
+                    if (i == 0)
+                    {
+                        return -1;
+                    }
+
+                    if (i <= -7 || i > 7)
+                    {
+                        return i;
+                    }
+
+                    if (i > -3 && i <= 3)
+                    {
+                        return i;
+                    }
+
+                    if (i <= -3)
+                    {
+                        return 10 + (i * 3);
+                    }
+                    else
+                    {
+                        return 10 - (i * 2);
+                    }
+                }
+            }
+            else
+            {
+                if (b2)
+                {
+                    if (i == 0)
+                    {
+                        return 1;
+                    }
+
+                    if (i < -8 || i >= 8)
+                    {
+                        return i - (i * i);
+                    }
+
+                    if (i > -4 && i <= 4)
+                    {
+                        return (i * i) - (i * i * i);
+                    }
+
+                    return i;
+                }
+                else
+                {
+                    if (i == 0)
+                    {
+                        return 1;
+                    }
+
+                    if (i <= -7 || i > 7)
+                    {
+                        return i - (i * i * i);
+                    }
+
+                    if (i >= -3 && i < 3)
+                    {
+                        return (i * i * i) - (i * i);
+                    }
+
+                    return i;
+                }
+            }
+        }
+    }
+}
