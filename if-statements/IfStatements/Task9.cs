8f04f4c66e9b08f1a056dc2887df34295f95ca06
L3b1n <binleo07@icloud.com>
2023-09-27 21:25:38 +0300

    Add task (if-statements) of educational practice

---
+++ if-statements/IfStatements/Task9.cs
@@ -0,0 +1,41 @@
+﻿namespace IfStatements
+{
+    public static class Task9
+    {
+        public static bool DoSomething(bool b, int i)
+        {
+            if (b)
+            {
+                if (i < -8 || i > 8)
+                {
+                    return true;
+                }
+
+                if (i >= -4 && i <= 4 && i != 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (i < -8 || i >= 8)
+                {
+                    return true;
+                }
+
+                if (i >= -4 && i < 4 && i != 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
