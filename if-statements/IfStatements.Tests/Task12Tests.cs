4dd2889ce9193a56b23379da12011978e19be447
L3b1n <binleo07@icloud.com>
2023-09-27 21:23:51 +0300

    Add tests for task (if-statements) of educational practice

---
+++ if-statements/IfStatements.Tests/Task12Tests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace IfStatements.Tests
+{
+    [TestFixture]
+    public class Task12Tests
+    {
+        [TestCase(0, ExpectedResult = 0)]
+        [TestCase(1, ExpectedResult = 0)]
+        [TestCase(2, ExpectedResult = 2)]
+        [TestCase(3, ExpectedResult = 6)]
+        [TestCase(4, ExpectedResult = 12)]
+        [TestCase(5, ExpectedResult = 5)]
+        [TestCase(6, ExpectedResult = 6)]
+        [TestCase(7, ExpectedResult = 7)]
+        [TestCase(8, ExpectedResult = 8)]
+        [TestCase(9, ExpectedResult = 9)]
+        [TestCase(10, ExpectedResult = -100)]
+        [TestCase(-1, ExpectedResult = 2)]
+        [TestCase(-2, ExpectedResult = 6)]
+        [TestCase(-3, ExpectedResult = 12)]
+        [TestCase(-4, ExpectedResult = 20)]
+        [TestCase(-5, ExpectedResult = 30)]
+        [TestCase(-6, ExpectedResult = -6)]
+        [TestCase(-7, ExpectedResult = -7)]
+        [TestCase(-8, ExpectedResult = -8)]
+        [TestCase(-9, ExpectedResult = 81)]
+        [TestCase(-10, ExpectedResult = 100)]
+        public int DoSomething_ReturnsBool(int i)
+        {
+            return Task12.DoSomething(i);
+        }
+    }
+}
