bdf75534859320984ee8a93d465f7fcc6fc3893c
L3b1n <binleo07@icloud.com>
2023-09-27 21:23:07 +0300

    Add tests for task (if-statements) of educational practice

---
+++ if-statements/IfStatements.Tests/Task4Tests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace IfStatements.Tests
+{
+    [TestFixture]
+    public class Task4Tests
+    {
+        [TestCase(true, true, ExpectedResult = false)]
+        [TestCase(false, true, ExpectedResult = true)]
+        [TestCase(true, false, ExpectedResult = true)]
+        [TestCase(false, false, ExpectedResult = false)]
+        public bool DoSomething1_ReturnsBool(bool b1, bool b2)
+        {
+            return Task4.DoSomething1(b1, b2);
+        }
+
+        [TestCase(true, true, ExpectedResult = false)]
+        [TestCase(false, true, ExpectedResult = true)]
+        [TestCase(true, false, ExpectedResult = true)]
+        [TestCase(false, false, ExpectedResult = false)]
+        public bool DoSomething2_ReturnsBool(bool b1, bool b2)
+        {
+            return Task4.DoSomething2(b1, b2);
+        }
+    }
+}
