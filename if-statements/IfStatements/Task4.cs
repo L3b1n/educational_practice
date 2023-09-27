33deb23457ffd03247292e740f0da73a1c156b8b
L3b1n <binleo07@icloud.com>
2023-09-27 21:25:12 +0300

    Add task (if-statements) of educational practice

---
+++ if-statements/IfStatements/Task4.cs
@@ -0,0 +1,15 @@
+﻿namespace IfStatements
+{
+    public static class Task4
+    {
+        public static bool DoSomething1(bool b1, bool b2)
+        {
+            return b1 ^ b2;
+        }
+
+        public static bool DoSomething2(bool b1, bool b2)
+        {
+           return b1 ^ b2;
+        }
+    }
+}
