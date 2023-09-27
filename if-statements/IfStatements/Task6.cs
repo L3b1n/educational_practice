94f022a1d473c2abe76b797e913f49a867ebe84b
L3b1n <binleo07@icloud.com>
2023-09-27 21:25:23 +0300

    Add task (if-statements) of educational practice

---
+++ if-statements/IfStatements/Task6.cs
@@ -0,0 +1,24 @@
+﻿namespace IfStatements
+{
+    public static class Task6
+    {
+        public static int DoSomething(int i)
+        {
+            if (i < -3 || i == 0 || i > 3)
+            {
+                return i;
+            }
+            else
+            {
+                if (i < 0)
+                {
+                    return i + (2 * i);
+                }
+                else
+                {
+                    return i - (i * i);
+                }
+            }
+        }
+    }
+}
