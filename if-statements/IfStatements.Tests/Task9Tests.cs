7641289a6a35c5f1be8409aeb822322649f2aa40
L3b1n <binleo07@icloud.com>
2023-09-27 21:23:33 +0300

    Add tests for task (if-statements) of educational practice

---
+++ if-statements/IfStatements.Tests/Task9Tests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+
+namespace IfStatements.Tests
+{
+    [TestFixture]
+    public class Task9Tests
+    {
+        [TestCase(true, 0, ExpectedResult = false)]
+        [TestCase(true, 1, ExpectedResult = true)]
+        [TestCase(true, 2, ExpectedResult = true)]
+        [TestCase(true, 3, ExpectedResult = true)]
+        [TestCase(true, 4, ExpectedResult = true)]
+        [TestCase(true, 5, ExpectedResult = false)]
+        [TestCase(true, 6, ExpectedResult = false)]
+        [TestCase(true, 7, ExpectedResult = false)]
+        [TestCase(true, 8, ExpectedResult = false)]
+        [TestCase(true, 9, ExpectedResult = true)]
+        [TestCase(true, 10, ExpectedResult = true)]
+        [TestCase(true, -1, ExpectedResult = true)]
+        [TestCase(true, -2, ExpectedResult = true)]
+        [TestCase(true, -3, ExpectedResult = true)]
+        [TestCase(true, -4, ExpectedResult = true)]
+        [TestCase(true, -5, ExpectedResult = false)]
+        [TestCase(true, -6, ExpectedResult = false)]
+        [TestCase(true, -7, ExpectedResult = false)]
+        [TestCase(true, -8, ExpectedResult = false)]
+        [TestCase(true, -9, ExpectedResult = true)]
+        [TestCase(true, -10, ExpectedResult = true)]
+        [TestCase(false, 0, ExpectedResult = false)]
+        [TestCase(false, 1, ExpectedResult = true)]
+        [TestCase(false, 2, ExpectedResult = true)]
+        [TestCase(false, 3, ExpectedResult = true)]
+        [TestCase(false, 4, ExpectedResult = false)]
+        [TestCase(false, 5, ExpectedResult = false)]
+        [TestCase(false, 6, ExpectedResult = false)]
+        [TestCase(false, 7, ExpectedResult = false)]
+        [TestCase(false, 8, ExpectedResult = true)]
+        [TestCase(false, 9, ExpectedResult = true)]
+        [TestCase(false, 10, ExpectedResult = true)]
+        [TestCase(false, -1, ExpectedResult = true)]
+        [TestCase(false, -2, ExpectedResult = true)]
+        [TestCase(false, -3, ExpectedResult = true)]
+        [TestCase(false, -4, ExpectedResult = true)]
+        [TestCase(false, -5, ExpectedResult = false)]
+        [TestCase(false, -6, ExpectedResult = false)]
+        [TestCase(false, -7, ExpectedResult = false)]
+        [TestCase(false, -8, ExpectedResult = false)]
+        [TestCase(false, -9, ExpectedResult = true)]
+        [TestCase(false, -10, ExpectedResult = true)]
+        public bool DoSomething_ReturnsBool(bool b, int i)
+        {
+            return Task9.DoSomething(b, i);
+        }
+    }
+}
