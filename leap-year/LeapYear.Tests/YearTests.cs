b7e61b588a81fd0433aff312ff17e8d177779e57
L3b1n <binleo07@icloud.com>
2023-09-28 10:58:05 +0300

    Add tests for task (leap-year) of educational practice

---
+++ leap-year/LeapYear.Tests/YearTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace LeapYearTask.Tests
+{
+    [TestFixture]
+    public class YearTests
+    {
+        [TestCase(2015)]
+        [TestCase(1970)]
+        [TestCase(2100)]
+        [TestCase(1800)]
+        [TestCase(1234)]
+        public void IsLeapYear_ReturnFalse(int year)
+        {
+            Assert.False(Year.IsLeapYear(year));
+        }
+
+        [TestCase(1996)]
+        [TestCase(2000)]
+        [TestCase(2020)]
+        [TestCase(2008)]
+        public void IsLeapYear_ReturnTrue(int year)
+        {
+            Assert.True(Year.IsLeapYear(year));
+        }
+    }
+}
