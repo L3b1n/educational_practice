1d63a26719720951baf1b752dbbea62a23924577
L3b1n <binleo07@icloud.com>
2023-09-28 10:59:06 +0300

    Add task (leap-year) of educational practice

---
+++ leap-year/LeapYear/Year.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LeapYearTask
+{
+    public static class Year
+    {
+        public static bool IsLeapYear(int year)
+        {
+            if (year % 100 == 0 && year % 400 == 0)
+            {
+                return true;
+            }
+            else if (year % 100 == 0)
+            {
+                return false;
+            }
+
+            if (year % 4 == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
