8366827efff925df8d25d9a087573bb48170ced0
L3b1n <binleo07@icloud.com>
2023-10-03 22:52:19 +0300

    Add task (numeric-conversions) of educational practice

---
+++ numeric-conversions/NumericConversions/ImplicitConversion.cs
@@ -0,0 +1,50 @@
+﻿namespace NumericConversions
+{
+    public static class ImplicitConversion
+    {
+        public static long IntToLong(int intValue)
+        {
+            return intValue;
+        }
+
+        public static float IntToFloat(int intValue)
+        {
+            return intValue;
+        }
+
+        public static double IntToDouble(int intValue)
+        {
+            return intValue;
+        }
+
+        public static decimal IntToDecimal(int intValue)
+        {
+            return intValue;
+        }
+
+        public static float LongToFloat(long longValue)
+        {
+            return longValue;
+        }
+
+        public static double LongToDouble(long longValue)
+        {
+            return longValue;
+        }
+
+        public static decimal LongToDecimal(long longValue)
+        {
+            return longValue;
+        }
+
+        public static short ByteToShort(byte byteValue)
+        {
+            return byteValue;
+        }
+
+        public static int ByteToInt(byte byteValue)
+        {
+            return byteValue;
+        }
+    }
+}
