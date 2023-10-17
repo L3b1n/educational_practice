7b512c8bb5730662671679778079f37db67e36f2
L3b1n <binleo07@icloud.com>
2023-10-17 22:51:40 +0300

    Add task (print-face) of educational practice

---
+++ print-face/Print/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PrintFace
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("  Hello, world! ");
+        }
+
+        public static void SayHelloUser(string userName)
+        {
+            Console.WriteLine($"  Hello, {userName} !");
+        }
+
+        public static void PrintFace()
+        {                                             /// Print this face.
+            Console.WriteLine(" +\"\"\"\"\"+");      ///   +"""""+
+            Console.WriteLine("(| o o |)");          ///  (| o o |)
+            Console.WriteLine(" |  ^  |");           ///   |  ^  |
+            Console.WriteLine(" | '-' |");           ///   | '-' |
+            Console.WriteLine(" +-----+");           ///   +-----+
+        }
+    }
+}
