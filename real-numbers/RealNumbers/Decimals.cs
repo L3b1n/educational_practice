9991c6359060cc520f3bab4eb9bb754c98b0cd46
L3b1n <binleo07@icloud.com>
2023-10-20 21:51:21 +0300

    Add task (real-numbers) of educational practice

---
+++ real-numbers/RealNumbers/Decimals.cs
@@ -0,0 +1,35 @@
+﻿namespace Literals
+{
+    public static class Decimals
+    {
+        public static decimal ReturnDecimal51()
+        {
+            return 0.0m;
+        }
+
+        public static decimal ReturnDecimal52()
+        {
+            return 0.0000001m;
+        }
+
+        public static decimal ReturnDecimal53()
+        {
+            return -10000.0000000001m;
+        }
+
+        public static decimal ReturnDecimal54()
+        {
+            return 1048294829438549029840452834.109492298482m;
+        }
+
+        public static decimal ReturnDecimal55()
+        {
+            return -30492996837502378502387459850.942692284652825m;
+        }
+
+        public static decimal ReturnDecimal56()
+        {
+            return 0.6m + 0.1m;
+        }
+    }
+}
