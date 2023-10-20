06db26d012648739c55fc6b06e5ca673b4fa8d84
L3b1n <binleo07@icloud.com>
2023-10-20 21:51:37 +0300

    Add task (real-numbers) of educational practice

---
+++ real-numbers/RealNumbers/Floats.cs
@@ -0,0 +1,57 @@
+﻿// ReSharper disable ConvertToConstant.Local
+
+namespace Literals
+{
+    public static class Floats
+    {
+        public static float ReturnFloat31()
+        {
+            return 0f;
+        }
+
+        public static float ReturnFloat32()
+        {
+            return 1.01f;
+        }
+
+        public static float ReturnFloat33()
+        {
+            return -0.01f;
+        }
+
+        public static float ReturnFloat34()
+        {
+            return 1048294829438549029840452834.109492298482f;
+        }
+
+        public static float ReturnFloat35()
+        {
+            return -30492996837502378502387459850243.942692284652825f;
+        }
+
+        public static float ReturnFloat36()
+        {
+            return -0.000000000000000000000000000000000000000123f;
+        }
+
+        public static float ReturnFloat37()
+        {
+            return -1.23E-40f;
+        }
+
+        public static float ReturnFloat38()
+        {
+            return 1048294829438549029840452834.109492298482f;
+        }
+
+        public static float ReturnFloat39()
+        {
+            return -30492996837502378502387459850243.942692284652825f;
+        }
+
+        public static float ReturnFloat310()
+        {
+            return 0.7f;
+        }
+    }
+}
