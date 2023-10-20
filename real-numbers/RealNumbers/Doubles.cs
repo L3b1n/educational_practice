81ff1bd1e9eb295518d43d88abb0b08d0177df23
L3b1n <binleo07@icloud.com>
2023-10-20 21:51:32 +0300

    Add task (real-numbers) of educational practice

---
+++ real-numbers/RealNumbers/Doubles.cs
@@ -0,0 +1,35 @@
+﻿namespace Literals
+{
+    public static class Doubles
+    {
+        public static double ReturnDouble41()
+        {
+            return 0.0;
+        }
+
+        public static double ReturnDouble42()
+        {
+            return 0.0001;
+        }
+
+        public static double ReturnDouble43()
+        {
+            return -10000.0000000001;
+        }
+
+        public static double ReturnDouble44()
+        {
+            return 1048294829438549029840452834.109492298482;
+        }
+
+        public static double ReturnDouble45()
+        {
+            return -30492996837502378502387459850243.942692284652825;
+        }
+
+        public static double ReturnDouble46()
+        {
+            return 0.6 + 0.1;
+        }
+    }
+}
