180a61b8c36dd7ee016f3d32a977955ca7e4c17e
L3b1n <binleo07@icloud.com>
2023-10-27 20:00:09 +0300

    Add task (selection-statments) of educational practice

---
+++ selection-statments/SelectionStatements/Month.cs
@@ -0,0 +1,18 @@
+﻿namespace SelectionStatments
+{
+    public enum Month
+    {
+        January = 1,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December,
+    }
+}
