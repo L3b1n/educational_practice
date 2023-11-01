b1e059e41bc30c16034e2998beb88795a61bca34
L3b1n <binleo07@icloud.com>
2023-11-01 22:45:29 +0300

    Add task (static-methods) of educational practice

---
+++ static-methods/StaticMethods/PassthroughMethods.cs
@@ -0,0 +1,60 @@
+﻿namespace StaticMethods
+{
+    public static class PassthroughMethods
+    {
+        public static int ReturnInt(int intParameter)
+        {
+            return intParameter;
+        }
+
+        public static uint ReturnUnsignedInt(uint uintParameter)
+        {
+            return uintParameter;
+        }
+
+        public static long ReturnLong(long longParameter)
+        {
+            return longParameter;
+        }
+
+        public static ulong ReturnUnsignedLong(ulong ulongParameter)
+        {
+            return ulongParameter;
+        }
+
+        public static float ReturnFloat(float floatParameter)
+        {
+            return floatParameter;
+        }
+
+        public static double ReturnDouble(double doubleParameter)
+        {
+            return doubleParameter;
+        }
+
+        public static decimal ReturnDecimal(decimal decimalParameter)
+        {
+            return decimalParameter;
+        }
+
+        public static string ReturnString(string stringParameter)
+        {
+            return stringParameter;
+        }
+
+        public static char ReturnChar(char charParameter)
+        {
+            return charParameter;
+        }
+
+        public static byte ReturnByte(byte byteParameter)
+        {
+            return byteParameter;
+        }
+
+        public static bool ReturnBool(bool boolParameter)
+        {
+            return boolParameter;
+        }
+    }
+}
