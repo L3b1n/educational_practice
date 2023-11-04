54db840568f3bfeb5aee75c2f5026211e6357822
L3b1n <binleo07@icloud.com>
2023-11-04 19:51:16 +0300

    Add task (static-methods-compiler-errors) of educational practice

---
+++ static-methods-compiler-errors/CompilerErrors/CS1739/MyClass.cs
@@ -0,0 +1,15 @@
+﻿namespace CompilerErrors.CS1739
+{
+    public static class MyClass
+    {
+        public static int MyMethod()
+        {
+            return ReturnInt(intParameter: 9_389_572);
+        }
+
+        public static int ReturnInt(int intParameter)
+        {
+            return intParameter;
+        }
+    }
+}
