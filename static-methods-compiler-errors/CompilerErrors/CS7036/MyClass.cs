9011f9a23cfb568bb81a958e7f48218def4361f2
L3b1n <binleo07@icloud.com>
2023-11-04 19:51:22 +0300

    Add task (static-methods-compiler-errors) of educational practice

---
+++ static-methods-compiler-errors/CompilerErrors/CS7036/MyClass.cs
@@ -0,0 +1,15 @@
+﻿namespace CompilerErrors.CS7036
+{
+    public static class MyClass
+    {
+        public static int MyMethod()
+        {
+            return ReturnInt(5_689_375);
+        }
+
+        public static int ReturnInt(int intParameter)
+        {
+            return intParameter;
+        }
+    }
+}
