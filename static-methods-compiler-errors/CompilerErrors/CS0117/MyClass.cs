7fc03baee4fa5db0e1ceea513f0b913729f191a8
L3b1n <binleo07@icloud.com>
2023-11-04 19:51:06 +0300

    Add task (static-methods-compiler-errors) of educational practice

---
+++ static-methods-compiler-errors/CompilerErrors/CS0117/MyClass.cs
@@ -0,0 +1,18 @@
+﻿namespace CompilerErrors.CS0117
+{
+    public static class MyClass
+    {
+        public static long MyMethod()
+        {
+            return AnotherClass.ReturnLong(longParameter: 49_023_471L);
+        }
+    }
+
+    public static class AnotherClass
+    {
+        public static long ReturnLong(long longParameter)
+        {
+            return longParameter;
+        }
+    }
+}
