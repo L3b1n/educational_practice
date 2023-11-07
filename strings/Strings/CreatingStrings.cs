eb1b26d1fa5ff6a4718218b17904eb125beaddb3
L3b1n <binleo07@icloud.com>
2023-11-07 23:04:24 +0300

    Add task (strings) of educational practice

---
+++ strings/Strings/CreatingStrings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Strings
+{
+    public static class CreatingStrings
+    {
+        public static string ReturnNewString(string str)
+        {
+            return new string(str);
+        }
+
+        public static string ReturnStringWithRepeatedChars(char c, int count)
+        {
+            return new string(c, count);
+        }
+
+        public static string ReturnStringFromCharArray(char[] c)
+        {
+            return new string(c);
+        }
+
+        public static string ReturnStringFromCharArray(char[] c, int startIndex, int length)
+        {
+            return new string(c, startIndex, length);
+        }
+    }
+}
