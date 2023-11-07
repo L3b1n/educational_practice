7b5995ece483db74c68322448812184e0fdb7271
L3b1n <binleo07@icloud.com>
2023-11-07 23:04:39 +0300

    Add task (strings) of educational practice

---
+++ strings/Strings/StringLiterals.cs
@@ -0,0 +1,30 @@
+﻿namespace Strings
+{
+    public static class StringLiterals
+    {
+        public static string ReturnAbcStringLiteral()
+        {
+            return "abc";
+        }
+
+        public static string ReturnAbcdefStringLiteral()
+        {
+            return "ABCDEF";
+        }
+
+        public static string ReturnEmptyString()
+        {
+            return string.Empty;
+        }
+
+        public static string ReturnFilePathStringLiteral()
+        {
+            return "c:\\documents\\files\\myfile0234.txt";
+        }
+
+        public static string ReturnFilePathVerbatimStringLiteral()
+        {
+            return @"c:\documents\files\myfile0234.txt";
+        }
+    }
+}
