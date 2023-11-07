115aa3e3a20b5fd3d3e89147bf497073a3d8aa94
L3b1n <binleo07@icloud.com>
2023-11-07 23:04:34 +0300

    Add task (strings) of educational practice

---
+++ strings/Strings/SplittingStrings.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Strings
+{
+    public static class SplittingStrings
+    {
+        public static string[] SplitCommaSeparatedString(string str)
+        {
+            return str.Split(',');
+        }
+
+        public static string[] SplitColonSeparatedString(string str)
+        {
+            return str.Split(':');
+        }
+
+        public static string[] SplitCommaSeparatedStringMaxTwoElements(string str)
+        {
+            return str.Split(',', 2);
+        }
+
+        public static string[] SplitColonSeparatedStringMaxThreeElements(string str)
+        {
+            return str.Split(':', 3);
+        }
+
+        public static string[] SplitHyphenSeparatedStringMaxThreeElementsRemoveEmptyStrings(string str)
+        {
+            return str.Split('-', 3, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public static string[] SplitColonAndCommaSeparatedStringMaxFourElementsRemoveEmptyStrings(string str)
+        {
+            char[] separators = new char[] { ',', ':' };
+            return str.Split(separators, 4, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public static string[] GetOnlyWords(string str)
+        {
+            char[] separators = new char[] { ',', ' ', '.', '!', '?', '\t', ':', ';', '-' };
+            return str.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public static string[] GetDataFromCsvLine(string str)
+        {
+            char[] separators = new char[] { ',', ' ' };
+            return str.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
