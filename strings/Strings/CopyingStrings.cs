a9f1649af3fd0ff209f75bd1c2b79cdd3d9bcbaf
L3b1n <binleo07@icloud.com>
2023-11-07 23:04:20 +0300

    Add task (strings) of educational practice

---
+++ strings/Strings/CopyingStrings.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Strings
+{
+    public static class CopyingStrings
+    {
+        public static string CopyOneChar(string source, string destination)
+        {
+            char[] destinationArray = destination.ToCharArray();
+
+            source.CopyTo(0, destinationArray, 4, source.Length);
+
+            return new string(destinationArray);
+        }
+
+        public static string CopyThreeChars(string source, string destination)
+        {
+            char[] destinationArray = destination.ToCharArray();
+
+            source.CopyTo(0, destinationArray, 0, source.Length);
+
+            return new string(destinationArray);
+        }
+
+        public static string CopyFiveChars(string source, string destination)
+        {
+            char[] destinationArray = destination.ToCharArray();
+
+            source.CopyTo(0, destinationArray, 4, source.Length);
+
+            return new string(destinationArray);
+        }
+
+        public static string CopySixChars(string source, string destination)
+        {
+            char[] destinationArray = destination.ToCharArray();
+
+            source.CopyTo(2, destinationArray, 5, 6);
+
+            return new string(destinationArray);
+        }
+
+        public static string GetProductionCode(string template, string regionCode, string locationCode, string dateCode, string factoryCode)
+        {
+            char[] destinationArray = template.ToCharArray();
+
+            regionCode.CopyTo(1, destinationArray, 0, 1);
+            locationCode.CopyTo(4, destinationArray, 3, 2);
+            dateCode.CopyTo(3, destinationArray, 7, 3);
+            factoryCode.CopyTo(2, destinationArray, 12, 4);
+
+            return new string(destinationArray);
+        }
+    }
+}
