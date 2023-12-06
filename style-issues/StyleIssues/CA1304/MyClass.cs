9276e6f287c479615f66a9a9d036a6496f959d52
L3b1n <binleo07@icloud.com>
2023-12-06 23:46:22 +0300

    Add task (style-issues) of educational practice

---
+++ style-issues/StyleIssues/CA1304/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace StyleIssues.CA1304
+{
+    public static class MyClass
+    {
+        public static string MyMethod(string str)
+        {
+            return "K-" + str.ToUpper(CultureInfo.InvariantCulture);
+        }
+    }
+}
