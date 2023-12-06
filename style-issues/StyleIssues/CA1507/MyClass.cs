2cb4a9fa31d7a52f072481b65d9cdbbe55c1e243
L3b1n <binleo07@icloud.com>
2023-12-06 23:46:32 +0300

    Add task (style-issues) of educational practice

---
+++ style-issues/StyleIssues/CA1507/MyClass.cs
@@ -0,0 +1,15 @@
+﻿namespace StyleIssues.CA1507
+{
+    public static class MyClass
+    {
+        public static string MyMethod(string str)
+        {
+            if (str == null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            return "test" + str;
+        }
+    }
+}
