5e24ca8f2746cc98d95604686666090f488a8db8
L3b1n <binleo07@icloud.com>
2023-12-06 23:46:37 +0300

    Add task (style-issues) of educational practice

---
+++ style-issues/StyleIssues/CA1707/MyClass.cs
@@ -0,0 +1,10 @@
+﻿namespace StyleIssues.CA1707
+{
+    public static class MyClass
+    {
+        public static string MyMethod(string myStr)
+        {
+            return myStr;
+        }
+    }
+}
