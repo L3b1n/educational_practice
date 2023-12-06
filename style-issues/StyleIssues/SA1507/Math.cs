814dc7997a41a26788a7379ce03661bc74c46e8c
L3b1n <binleo07@icloud.com>
2023-12-06 23:48:03 +0300

    Add task (style-issues) of educational practice

---
+++ style-issues/StyleIssues/SA1507/Math.cs
@@ -0,0 +1,11 @@
+﻿namespace StyleIssues.SA1507
+{
+    public static class Math
+    {
+        public static int Sum(int x, int y)
+        {
+            int result = x + y;
+            return result;
+        }
+    }
+}
