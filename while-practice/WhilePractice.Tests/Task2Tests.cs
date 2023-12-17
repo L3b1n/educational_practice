b570dec666ff97bac1a06e3cca775c4872702bf1
L3b1n <binleo07@icloud.com>
2023-12-17 21:40:44 +0300

    Add tests for task (while-practice) of educational practice

---
+++ while-practice/WhilePractice.Tests/Task2Tests.cs
@@ -0,0 +1,17 @@
+﻿namespace WhilePractice.Tests
+{
+    public class Task2Tests
+    {
+        [TestCase(6, 0.3761904761904762)]
+        [TestCase(10, 0.3821789321789322)]
+        [TestCase(100, 0.3862453487194004)]
+        [TestCase(1_000_000, 0.38629436111938714)]
+        [TestCase(10_000_000, 0.38629436111988125)]
+        public void GetSumTwoTests(int n, double expectedResult)
+        {
+            double actualResult = Task2.SumSequenceElements(n);
+
+            Assert.AreEqual(expectedResult, actualResult, double.Epsilon);
+        }
+    }
+}
