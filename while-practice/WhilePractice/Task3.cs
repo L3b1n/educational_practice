146989d61b879a752307d51ae3972bf436d23997
L3b1n <binleo07@icloud.com>
2023-12-17 21:46:35 +0300

    Add task (while-practice) of educational practice

---
+++ while-practice/WhilePractice/Task3.cs
@@ -0,0 +1,19 @@
+﻿namespace WhilePractice
+{
+    public static class Task3
+    {
+        public static double SumSequenceElements(int n)
+        {
+            double sum = 0;
+            double i = 1;
+
+            while (i <= n)
+            {
+                sum += 1d / (i * i * i * i * i);
+                i++;
+            }
+
+            return sum;
+        }
+    }
+}
