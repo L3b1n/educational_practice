eaf35e77172c56f2f64fb99e3fc588122600a20e
L3b1n <binleo07@icloud.com>
2023-12-17 21:46:46 +0300

    Add task (while-practice) of educational practice

---
+++ while-practice/WhilePractice/Task5.cs
@@ -0,0 +1,19 @@
+﻿namespace WhilePractice
+{
+    public static class Task5
+    {
+        public static double GetSequenceProduct(int n)
+        {
+            double prod = 1;
+            double i = 1;
+
+            while (i <= n)
+            {
+                prod *= 1d + (1d / (i * i));
+                i++;
+            }
+
+            return prod;
+        }
+    }
+}
