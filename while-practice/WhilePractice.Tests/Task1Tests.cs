4f57f1c43d8042d651d78ef05d55f1c0d0db2e82
L3b1n <binleo07@icloud.com>
2023-12-17 21:40:33 +0300

    Add tests for task (while-practice) of educational practice

---
+++ while-practice/WhilePractice.Tests/Task1Tests.cs
@@ -0,0 +1,19 @@
+﻿global using NUnit.Framework;
+
+namespace WhilePractice.Tests
+{
+    public class Task1Tests
+    {
+        [TestCase(6, 2.4499999999999997)]
+        [TestCase(10, 2.9289682539682538)]
+        [TestCase(100, 5.187377517639621)]
+        [TestCase(1_000_000, 14.392726722864989)]
+        [TestCase(10_000_000, 16.695311365857272)]
+        public void GetSumOneTests(int n, double expectedResult)
+        {
+            double actualResult = Task1.SumSequenceElements(n);
+
+            Assert.AreEqual(expectedResult, actualResult, double.Epsilon);
+        }
+    }
+}
