a62fc9a687fb7391213d5df3f2258309d13e5bc1
L3b1n <binleo07@icloud.com>
2023-12-23 22:56:35 +0300

    Add task (while-statements) of educational practice

---
+++ while-statements/WhileStatements/Digits.cs
@@ -0,0 +1,35 @@
+﻿namespace WhileStatements
+{
+    public static class Digits
+    {
+        public static ulong GetDigitsSum(ulong n)
+        {
+            ulong sumnum = 0;
+            while (n > 0)
+            {
+                sumnum += n % 10;
+                n /= 10;
+            }
+
+            return sumnum;
+        }
+
+        public static ulong GetDigitsProduct(ulong n)
+        {
+            ulong sumnum = 1;
+
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            while (n > 0)
+            {
+                sumnum *= n % 10;
+                n /= 10;
+            }
+
+            return sumnum;
+        }
+    }
+}
