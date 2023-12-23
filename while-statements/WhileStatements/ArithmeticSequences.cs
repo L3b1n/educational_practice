7da79087e42a72697ce3da34ed50df18c32c9f64
L3b1n <binleo07@icloud.com>
2023-12-23 22:56:26 +0300

    Add task (while-statements) of educational practice

---
+++ while-statements/WhileStatements/ArithmeticSequences.cs
@@ -0,0 +1,61 @@
+﻿namespace WhileStatements
+{
+    public static class ArithmeticSequences
+    {
+        public static int SumArithmeticSequenceTerms1(int a, int n)
+        {
+            int sum = 0, i = 0;
+
+            while (i < n)
+            {
+                sum += a + i;
+                i++;
+            }
+
+            return sum;
+        }
+
+        public static int SumArithmeticSequenceTerms2(int n)
+        {
+            const int firstTerm = 17;
+            const int commonDifference = 33;
+
+            int sum = 0, i = 0;
+
+            while (i < n)
+            {
+                sum += firstTerm + (i * commonDifference);
+                i++;
+            }
+
+            return sum;
+        }
+
+        public static int SumArithmeticSequenceTerms3(int a, int n)
+        {
+            int sum = 0, i = 0;
+
+            while (i < n)
+            {
+                sum += a + (3 * i);
+                i++;
+            }
+
+            return sum;
+        }
+
+        public static int SumArithmeticSequenceTerms4(int a, int d, int n)
+        {
+            int sum = 0, i = 0;
+
+            while (i < n)
+            {
+                sum += a;
+                a += d;
+                i++;
+            }
+
+            return sum;
+        }
+    }
+}
